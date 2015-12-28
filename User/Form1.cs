a5145ccf2050b39f87f4f4015b6e4cc965339792
MateuszWojtys <matwitek@gmail.com>
2015-12-28 12:11:11 +0100

    Stworzony projket dla aplikacji klienta, możliwość połączenia z Loggerem i wysłanie testowej wiadomości

---
+++ User/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace User
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Metoda łącząca się z Loggerem
+        public void connectAndSendToLogger()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+            BinaryWriter bw = new BinaryWriter(stream);
+            bw.Write(DateTime.Now.ToString() );
+            bw.Write("User");
+            bw.Write("Bank");
+            bw.Write("Tymczasowy tekst ");
+            bw.Close();
+            stream.Close();
+            client.Close();
+            
+        }
+
+      
+        //Przycisk odpowiedzialny za wysyłanie
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connectAndSendToLogger();
+        }
+    }
+}
