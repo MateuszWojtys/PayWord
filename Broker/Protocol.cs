9087fc1730c1e5af3f96ffc51f28c9dd9bed4b58
MateuszWojtys <matwitek@gmail.com>
2016-01-06 02:59:42 +0100

    edytowany protokół

--- Broker/Protocol.cs
+++ Broker/Protocol.cs
@@ -14,5 +14,8 @@ namespace Broker
         public static string REGISTRATION = "#Registration"; //Wysyłane przez klienta do banku podczas żądanie rejestracji w systemie
         public static string LOGINVERIFY = "#LogInVerify"; //Wysyłane przez bank do klienta - wynik weryfikacji logowania
         public static string REGISTRATIONVERIFY = "#RegistrationVerify";// Wysyłane przez bank do klienta - wynik rejestracji
+        public static string FIRSTPAYMENT = "#FirstPayment"; // Wysyłane przez klienta do sprzedawcy podczas 1 platnosci (wysyla commitment)
+        public static string PAYMENT = "#Payment"; // Wysyłane przez klienta do sprzedawcy jako nagłówek płatności
+        public static string PAYMENTVERIFY = "#PaymentVerify"; //Wysyłane przez sprzedawce do klienta - wynik weryfikacji płatności
     }
 }
