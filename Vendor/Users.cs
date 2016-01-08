2699fa9a18633908a82a84858da469d924d89e2e
MateuszWojtys <matwitek@gmail.com>
2016-01-09 00:58:48 +0100

    zamiast firstpayment - commitment, zrobiona do konca obsluga platnosci, tworzenie raportu, raport gotowy do wyslania do Brokera

--- Vendor/Users.cs
+++ Vendor/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,45 @@ namespace Vendor
 {
     public class Users
     {
+
+         public DataTable dt;
+
+        public Users()
+         {
+             createUsersTable();
+         }
+        public void createUsersTable()
+        {
+            DataColumn[] tmp = new DataColumn[1];
+            dt = new DataTable("Użytkownicy");
+            DataColumn name = new DataColumn("Nazwa klienta", typeof(string));
+           // DataColumn certificateNumber = new DataColumn("Numer certyfikatu", typeof(string));????????????????
+            DataColumn lastPaymentCoin = new DataColumn("Ostatnia przesłana moneta", typeof(string));
+            DataColumn numberOfSpentCoins = new DataColumn("Ilość wydanych monet w sumie", typeof(string));
+
+            dt.Columns.Add(name);
+            dt.Columns.Add(lastPaymentCoin);
+            dt.Columns.Add(numberOfSpentCoins);
+            tmp[0] = name;
+            dt.PrimaryKey = tmp;
+        }
+
+        public void updateDataTable(UsersData ud, string[] issuedCoins)
+        {
+            DataRow foundRow = dt.Rows.Find(issuedCoins[0]);
+            foundRow["Ostatnia przesłana moneta"] = ud.lastPayment[0];
+            string newValue = (Convert.ToInt32(foundRow["Ilość wydanych monet w sumie"]) + Convert.ToInt32(issuedCoins[1])).ToString();
+            foundRow["Ilość wydanych monet w sumie"] = newValue; 
+        }
+       public void addNewRecord(UsersData ud)
+        {
+            DataRow row = dt.NewRow();
+            row["Nazwa klienta"] = ud.userName;
+            row["Ostatnia przesłana moneta"] = ud.lastPayment[0];
+            row["Ilość wydanych monet w sumie"] = ud.lastPayment[1];
+            dt.Rows.Add(row);
+        }
+
         public struct UsersData
         {
             public string userName;
@@ -31,6 +71,9 @@ namespace Vendor
             //klucz publiczny usera??????????????????????????????????????????????????????????????????????
             public DateTime expirationDate;// data wygaśnięcia certyfikatu
         }
+
+
+
     }
 
     
--- Vendor/Vendor.Designer.cs
+++ Vendor/Vendor.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridViewUsersData = new System.Windows.Forms.DataGridView();
+            this.buttonShowReport = new System.Windows.Forms.Button();
+            this.buttonSendReport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsersData)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridViewUsersData
+            // 
+            this.dataGridViewUsersData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsersData.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewUsersData.Name = "dataGridViewUsersData";
+            this.dataGridViewUsersData.ReadOnly = true;
+            this.dataGridViewUsersData.Size = new System.Drawing.Size(336, 149);
+            this.dataGridViewUsersData.TabIndex = 0;
+            // 
+            // buttonShowReport
+            // 
+            this.buttonShowReport.Location = new System.Drawing.Point(342, 12);
+            this.buttonShowReport.Name = "buttonShowReport";
+            this.buttonShowReport.Size = new System.Drawing.Size(107, 23);
+            this.buttonShowReport.TabIndex = 1;
+            this.buttonShowReport.Text = "Pokaż raport";
+            this.buttonShowReport.UseVisualStyleBackColor = true;
+            this.buttonShowReport.Click += new System.EventHandler(this.buttonShowReport_Click);
+            // 
+            // buttonSendReport
+            // 
+            this.buttonSendReport.Location = new System.Drawing.Point(342, 41);
+            this.buttonSendReport.Name = "buttonSendReport";
+            this.buttonSendReport.Size = new System.Drawing.Size(107, 23);
+            this.buttonSendReport.TabIndex = 2;
+            this.buttonSendReport.Text = "Wyślij raport";
+            this.buttonSendReport.UseVisualStyleBackColor = true;
             // 
             // Vendor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 238);
+            this.ClientSize = new System.Drawing.Size(458, 149);
+            this.Controls.Add(this.buttonSendReport);
+            this.Controls.Add(this.buttonShowReport);
+            this.Controls.Add(this.dataGridViewUsersData);
             this.Name = "Vendor";
             this.Text = "Vendor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsersData)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewUsersData;
+        private System.Windows.Forms.Button buttonShowReport;
+        private System.Windows.Forms.Button buttonSendReport;
     }
 }
 
